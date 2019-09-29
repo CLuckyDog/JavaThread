ad49755d0eff555051775a934e6c6a21740d33d5
Alion <406662248@qq.com>
2019-09-29 21:33:37 +0800

    arraylist和map的线程安全性

---
+++ src/main/java/com/rh/chapter_two/ArrayListMultiThread.java
@@ -0,0 +1,43 @@
+package com.rh.chapter_two;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Administrator on 2019/9/29.
+ * 并发下的ArrayList
+ *
+ * 可能有三种结果。
+ * 1，正常结果，2000000。
+ * 2，报错。
+ *          因为，ArrayList在扩容过程中，内部一致性被破坏
+ *          但由于没有锁的保护，另外一个线程访问到了不一致的内部状态，导致出现越界问题。
+ * 3，不报错，但是得到一个小于2000000的数值。
+ *
+ * 优化：加sync锁即可解决问题。扩容错误可以在创建时不传入10即可避免。
+ */
+public class ArrayListMultiThread {
+//    static ArrayList<Integer> arrayList=new ArrayList<>(10);
+    static ArrayList<Integer> arrayList=new ArrayList<>();
+
+    public static class AddThread implements Runnable{
+        @Override
+        public void run() {
+            synchronized (AddThread.class){
+                for(int i=0;i<1000000;i++){
+                    arrayList.add(i);
+                }
+            }
+
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1=new Thread(new AddThread());
+        Thread t2=new Thread(new AddThread());
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(arrayList.size());
+    }
+}
---
+++ src/main/java/com/rh/chapter_two/HashMapMultiThread.java
@@ -0,0 +1,36 @@
+package com.rh.chapter_two;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2019/9/29.
+ *
+ * HashMap的线程安全性验证
+ */
+public class HashMapMultiThread {
+    static Map<String,String> map=new HashMap<>();
+
+    public static class AddThread implements Runnable{
+        int start=0;
+        public AddThread(int start){
+            this.start=start;
+        }
+        @Override
+        public void run() {
+            for (int i=start;i<100000;i+=2){
+                map.put(Integer.toString(i),Integer.toBinaryString(i));
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1=new Thread(new HashMapMultiThread.AddThread(0));
+        Thread t2=new Thread(new HashMapMultiThread.AddThread(1));
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(map.size());
+    }
+}
