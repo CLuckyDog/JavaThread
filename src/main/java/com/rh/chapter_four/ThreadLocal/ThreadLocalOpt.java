7b39d74fc0bb1944d7801fa67da47c7ad6ff04fa
pan_zhongjian <pan_zhongjian@hoperun.com>
2019-10-23 17:23:59 +0800

    ThreadLocal示例代码提交

--- src/main/java/com/rh/chapter_four/ThreadLocal/ThreadLocalOpt.java
+++ src/main/java/com/rh/chapter_four/ThreadLocal/ThreadLocalOpt.java
@@ -9,12 +9,12 @@ import java.util.concurrent.Executors;
 /**
  * @author ：pan_zhongjian
  * @version :
- * @date ：Created in 2019/10/22 15:42
- * @description: ThreadLocal的简单使用
+ * @date ：Created in 2019/10/22 16:52
+ * @description:
  * @modified By:
  */
-public class ThreadLocalDemo {
-    private static final SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+public class ThreadLocalOpt {
+    static ThreadLocal<SimpleDateFormat> tl=new ThreadLocal<>();
     public static class ParseDate implements Runnable{
         int i=0;
 
@@ -26,13 +26,13 @@ public class ThreadLocalDemo {
         public void run() {
             try {
                 /*
-                * 这里会报错：
-                * Exception in thread "pool-1-thread-96"
-                * java.lang.NumberFormatException: For input string: ""
-                * 出现这个问题的原因，是SimpleDateFormat.parse()方法不是线程安全的，
-                * 因此，在线程池中共享这个对象必然导致错误。
+                * 如果当前线程不持有SimpleDateFormat对象实例
+                * 那就新建一个并把它设置到当前线程中，如果有，则直接使用
                 * */
-                Date t=sdf.parse("2019-10-22 15:50:"+i%60);
+                if (tl.get()==null){
+                    tl.set(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+                }
+                Date t=tl.get().parse("2019-10-22 15:50:"+i%60);
                 System.out.println(i+":"+t);
             } catch (ParseException e) {
                 e.printStackTrace();
@@ -40,11 +40,11 @@ public class ThreadLocalDemo {
         }
     }
 
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
         ExecutorService es= Executors.newFixedThreadPool(10);
         for(int i=0;i<1000;i++){
             es.execute(new ParseDate(i));
         }
-//        sdf.parse("");
     }
+
 }
