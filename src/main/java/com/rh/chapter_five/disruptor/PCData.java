df7bae9c80cf3b02b3cc18c13e88f4e99cf6f78c
pan_zhongjian <pan_zhongjian@hoperun.com>
2019-11-07 16:01:11 +0800

    提交disruptor实现生产者消费者模式

---
+++ src/main/java/com/rh/chapter_five/disruptor/PCData.java
@@ -0,0 +1,20 @@
+package com.rh.chapter_five.disruptor;
+
+/**
+ * @author ：pan_zhongjian
+ * @version :
+ * @date ：Created in 2019/11/7 14:57
+ * @description:
+ * @modified By:
+ */
+public class PCData {
+    private long value;
+
+    public long getValue() {
+        return value;
+    }
+
+    public void setValue(long value) {
+        this.value = value;
+    }
+}
