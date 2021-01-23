e2c1cd5fa872c809ec67946b6a4e932e4c5a6a09
Alion <406662248@qq.com>
2021-01-23 22:24:54 +0800

    提交视频学习代码

---
+++ src/main/java/com/rh/vedio/MyBenchmark.java
@@ -0,0 +1,85 @@
+package com.rh.vedio;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.Arrays;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 练习JMH工具的使用
+ */
+@Fork(1)//代表启动多个单独的进程分别测试每个方法，我们这里指定为每个方法启动一个进程。
+// 测试方法平均执行时间
+@BenchmarkMode(Mode.AverageTime)
+@Warmup(iterations=3)//热身次数，3次
+@Measurement(iterations=5)//5轮测试
+//@OutputTimeUnit(TimeUnit.MICROSECONDS) // 输出结果的时间粒度为微秒
+public class MyBenchmark {
+    static int[] ARRAY = new int[1000_000_00];
+    static {
+        Arrays.fill(ARRAY, 1);
+    }
+
+    @Benchmark
+    public int c() throws Exception {
+        int[] array = ARRAY;
+        FutureTask<Integer> t1 = new FutureTask<>(()->{
+            int sum = 0;
+            for(int i = 0; i < 250_000_00;i++) {
+                sum += array[0+i];
+            }
+            return sum;
+        });
+        FutureTask<Integer> t2 = new FutureTask<>(()->{
+            int sum = 0;
+            for(int i = 0; i < 250_000_00;i++) {
+                sum += array[250_000_00+i];
+            }
+            return sum;
+        });
+        FutureTask<Integer> t3 = new FutureTask<>(()->{
+            int sum = 0;
+            for(int i = 0; i < 250_000_00;i++) {
+                sum += array[500_000_00+i];
+            }
+            return sum;
+        });
+        FutureTask<Integer> t4 = new FutureTask<>(()->{
+            int sum = 0;
+            for(int i = 0; i < 250_000_00;i++) {
+                sum += array[750_000_00+i];
+            }
+            return sum;
+        });
+        new Thread(t1).start();
+        new Thread(t2).start();
+        new Thread(t3).start();
+        new Thread(t4).start();
+        return t1.get() + t2.get() + t3.get()+ t4.get();
+    }
+    @Benchmark
+    public int d() throws Exception {
+        int[] array = ARRAY;
+        FutureTask<Integer> t1 = new FutureTask<>(()->{
+            int sum = 0;
+            for(int i = 0; i < 1000_000_00;i++) {
+                sum += array[0+i];
+            }
+            return sum;
+        });
+        new Thread(t1).start();
+        return t1.get();
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(MyBenchmark.class.getSimpleName())
+                .build();
+        new Runner(opt).run();
+    }
+}
---
+++ src/main/java/com/rh/vedio/Sync.java
@@ -0,0 +1,10 @@
+package com.rh.vedio;
+
+import org.apache.log4j.Logger;
+
+public class Sync {
+    public static final Logger logger = Logger.getLogger(Sync.class);
+    public static void main(String[] args) {
+        logger.debug("xxxx");
+    }
+}
