ea4c490ee111c31b6a4b37aa9780c0093553ede0
Alion <406662248@qq.com>
2021-01-24 18:19:49 +0800

    彻底解决lombok使用报错

--- src/main/java/com/rh/vedio/Sync.java
+++ src/main/java/com/rh/vedio/Sync.java
@@ -1,14 +1,10 @@
 package com.rh.vedio;
 
-import org.apache.log4j.Logger;
+import lombok.extern.slf4j.Slf4j;
 
-//import lombok.extern.slf4j.Slf4j;
-
-//@Slf4j(topic = "c.Sync")
+@Slf4j(topic = "c.Sync")
 public class Sync {
-    private static final Logger logger = Logger.getLogger(Sync.class);
     public static void main(String[] args) {
-//        log.debug("11");
-        logger.debug("11");
+        log.debug("11");
     }
 }
