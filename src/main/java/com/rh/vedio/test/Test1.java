f8f7b448fddf7ff11eb3f44c14237251428ce0ac
Alion <406662248@qq.com>
2021-01-26 22:09:26 +0800

    提交家里代码

---
+++ src/main/java/com/rh/vedio/test/Test1.java
@@ -0,0 +1,25 @@
+package com.rh.vedio.test;
+
+public class Test1 {
+    public static void main(String[] args) {
+        new Thread(() -> {
+            while(true) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "t1").start();
+
+        new Thread(() -> {
+            while(true) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "t2").start();
+    }
+}
