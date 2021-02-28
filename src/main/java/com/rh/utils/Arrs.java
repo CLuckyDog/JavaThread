0ba98df1a1dbd7afd988a900010b06acd59155e0
Alion <406662248@qq.com>
2021-02-28 21:06:30 +0800

    提交第六章代码，原子数组

---
+++ src/main/java/com/rh/utils/Arrs.java
@@ -0,0 +1,13 @@
+package com.rh.utils;
+
+public class Arrs {
+    public static void main(String[] args) {
+        int[] num = new int[90];
+        for (int i = 0; i <90 ; i++) {
+            num[i] = i+1;
+            System.out.println(num[i]);
+        }
+        int[] xs= new int[100];
+
+    }
+}
