6fd2b9f34a62430224ac3e4596a93e5638e06632
Alion <406662248@qq.com>
2021-05-05 17:21:58 +0800

    不用volatile修饰变量，来使用CAS的方式

--- src/main/java/com/rh/bilibili/Person.java
+++ src/main/java/com/rh/bilibili/Person.java
@@ -2,4 +2,5 @@ package com.rh.bilibili;
 
 public class Person {
     public volatile int value = 0;
+    public int num=0;
 }
--- src/main/java/com/rh/bilibili/TestUnsafeCAS.java
+++ src/main/java/com/rh/bilibili/TestUnsafeCAS.java
@@ -5,11 +5,13 @@ import sun.misc.Unsafe;
 
 public class TestUnsafeCAS {
     private static final long valueOffset;
+    private static final long numOffset;
     static final Unsafe UNSAFE;
     static {
         UNSAFE= UnsafeAccessor.getUnsafe();
         try {
             valueOffset=UNSAFE.objectFieldOffset(Person.class.getDeclaredField("value"));
+            numOffset=UNSAFE.objectFieldOffset(Person.class.getDeclaredField("num"));
         } catch (NoSuchFieldException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
@@ -22,17 +24,21 @@ public class TestUnsafeCAS {
             int i=0;
             for (i = 0; i < 10000; i++) {
                 while (true){
-                    int pre = person.value;
+                    //获取value的操作，一定要放在while里面，否则可能导致死循环
+//                    int pre = person.value;
+                    //另一种CAS用法，基本数据类型变量不需要添加volatile修饰
+                    int pre = UNSAFE.getIntVolatile(person, numOffset);
                     int nextVal = pre +1;
                     System.out.println("while aaa");
-                    if (UNSAFE.compareAndSwapInt(person, valueOffset, pre, nextVal)){
+                    if (UNSAFE.compareAndSwapInt(person, numOffset, pre, nextVal)){
                         System.out.println("====="+nextVal);
                         break;
                     }
                 }
             }
             System.out.println("++++"+i);
-            System.out.println(Thread.currentThread().getName()+"###"+person.value);
+//            System.out.println(Thread.currentThread().getName()+"###"+person.value);
+            System.out.println(Thread.currentThread().getName()+"###"+UNSAFE.getIntVolatile(person, numOffset));
         }).start();
 
         new Thread(()->{
@@ -40,17 +46,20 @@ public class TestUnsafeCAS {
             for (i = 0; i < 10000; i++) {
                 while (true){
                     //获取value的操作，一定要放在while里面，否则可能导致死循环
-                    int pre = person.value;
+//                    int pre = person.value;
+                    //另一种CAS用法，基本数据类型变量不需要添加volatile修饰
+                    int pre = UNSAFE.getIntVolatile(person, numOffset);
                     int nextVal = pre +1;
                     System.out.println("while bbb");
-                    if (UNSAFE.compareAndSwapInt(person, valueOffset, pre, nextVal)){
+                    if (UNSAFE.compareAndSwapInt(person, numOffset, pre, nextVal)){
                         System.out.println(">>>>>"+nextVal);
                         break;
                     }
                 }
             }
             System.out.println("----"+i);
-            System.out.println(Thread.currentThread().getName()+"@@@"+person.value);
+//            System.out.println(Thread.currentThread().getName()+"@@@"+person.value);
+            System.out.println(Thread.currentThread().getName()+"@@@"+UNSAFE.getIntVolatile(person, numOffset));
         }).start();
 //        Thread.sleep(1000);
 
