694d5343bad87a2ef96b4c7b0dd00b1b20558b52
Alion <406662248@qq.com>
2021-05-05 16:51:18 +0800

    提交unsafe测试循环增加value值案例

---
+++ src/main/java/com/rh/bilibili/Person.java
@@ -0,0 +1,5 @@
+package com.rh.bilibili;
+
+public class Person {
+    public volatile int value = 0;
+}
---
+++ src/main/java/com/rh/bilibili/TestUnsafeCAS.java
@@ -0,0 +1,58 @@
+package com.rh.bilibili;
+
+import com.rh.bilibili.utils.UnsafeAccessor;
+import sun.misc.Unsafe;
+
+public class TestUnsafeCAS {
+    private static final long valueOffset;
+    static final Unsafe UNSAFE;
+    static {
+        UNSAFE= UnsafeAccessor.getUnsafe();
+        try {
+            valueOffset=UNSAFE.objectFieldOffset(Person.class.getDeclaredField("value"));
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Person person = new Person();
+        new Thread(()->{
+            int i=0;
+            for (i = 0; i < 10000; i++) {
+                while (true){
+                    int pre = person.value;
+                    int nextVal = pre +1;
+                    System.out.println("while aaa");
+                    if (UNSAFE.compareAndSwapInt(person, valueOffset, pre, nextVal)){
+                        System.out.println("====="+nextVal);
+                        break;
+                    }
+                }
+            }
+            System.out.println("++++"+i);
+            System.out.println(Thread.currentThread().getName()+"###"+person.value);
+        }).start();
+
+        new Thread(()->{
+            int i=0;
+            for (i = 0; i < 10000; i++) {
+                while (true){
+                    int pre = person.value;
+                    int nextVal = pre +1;
+                    System.out.println("while bbb");
+                    if (UNSAFE.compareAndSwapInt(person, valueOffset, pre, nextVal)){
+                        System.out.println(">>>>>"+nextVal);
+                        break;
+                    }
+                }
+            }
+            System.out.println("----"+i);
+            System.out.println(Thread.currentThread().getName()+"@@@"+person.value);
+        }).start();
+//        Thread.sleep(1000);
+
+    }
+
+}
