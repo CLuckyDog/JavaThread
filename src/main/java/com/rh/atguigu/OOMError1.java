6d897307d3c94dca4c3acaf94af3eb21ef56dec3
unknown <Administrator@note190342.Hoperun.com>
2021-10-14 21:30:26 +0800

    提交JVM案例代码

---
+++ src/main/java/com/rh/atguigu/OOMError1.java
@@ -0,0 +1,12 @@
+package com.rh.atguigu;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: pzj
+ * \* Date: 2021/10/14
+ * \* Time: 15:39
+ * \* Description:
+ * \
+ */
+public class OOMError1 {
+}
---
+++ src/main/java/com/rh/atguigu/WeakHashMapDemo.java
@@ -0,0 +1,12 @@
+package com.rh.atguigu;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: pzj
+ * \* Date: 2021/10/14
+ * \* Time: 15:04
+ * \* Description:
+ * \
+ */
+public class WeakHashMapDemo {
+}
